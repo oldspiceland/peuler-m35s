459e99f6cac2fd486f5bb558950b301bf4f6877c
George Spiceland <georgespiceland@gmail.com>
2016-08-07 00:55:43 -0400

    Updated comments for sanity

--- main.go
+++ main.go
@@ -4,6 +4,6 @@ import (
 	"github.com/oldspiceland/peuler-m35s/multieuler"
 )
 
-func main() {
+func main() { //TODO: Add arguments to remove hard coded limits feeding into functions
 	multieuler.Multiples(3, 5, 1000)
 }
--- multieuler/multieuler.go
+++ multieuler/multieuler.go
@@ -2,13 +2,14 @@ package multieuler
 
 import "fmt"
 
-func Multiples(x int, y int, limit int) {
-	for i := 0; i < limit; i++ {
-		if i%x == 0 {
+func Multiples(x int, y int, limit int) { //Multiples takes 3 int args
+	for i := 0; i < limit; i++ { //Loops until 'limit' arg reached TODO: Currently missing last hit
+		if i%x == 0 { //Checks if current number is a multiple of x
 			fmt.Printf("%v is a multiple of %v\n", i, x)
 		}
-		if i%y == 0 {
+		if i%y == 0 { //Checks if current number is a multiple of y
 			fmt.Printf("%v is a multiple of %v\n", i, y)
 		}
 	}
+	//This is not a very elegant solution, and does not provide an elegant output
 }
