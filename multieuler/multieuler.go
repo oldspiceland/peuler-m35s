c2a495607a71a36285d9b34c0c25136ad13dc3c1
George Spiceland <georgespiceland@gmail.com>
2016-08-07 13:04:38 -0400

    more work on breaking down output

--- multieuler/multieuler.go
+++ multieuler/multieuler.go
@@ -8,25 +8,22 @@ func Multiples(x int, y int, limit int) { //Multiples takes 3 int argsa
 	for i := 1; i <= limit; i++ { //Loops through 'limit' arg
 		if i%x == 0 { //Checks if current number is a multiple of x
 			xMult = true
-			fmt.Printf("%v is a multiple of %v\n", i, x)
 		} else {
 			xMult = false
 		}
 		if i%y == 0 { //Checks if current number is a multiple of y
 			yMult = true
-			fmt.Printf("%v is a multiple of %v\n", i, y)
 		} else {
 			yMult = false
 		}
 		switch {
 		case xMult == true && yMult == true:
-
+			fmt.Printf("%v is a multiple of %v and %v\n", i, x, y)
 		case xMult == true && yMult == false:
-
+			fmt.Printf("%v is a multiple of %v\n", i, x)
 		case xMult == false && yMult == true:
-
+			fmt.Printf("%v is a multiple of %v\n", i, y)
 		case xMult == false && yMult == false:
-
 		}
 	}
 	//This is not a very elegant solution, and does not provide an elegant output
