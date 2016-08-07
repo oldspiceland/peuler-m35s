6dade051d34058762a6d0769f93aed0cb46eab15
George Spiceland <georgespiceland@gmail.com>
2016-08-07 12:26:59 -0400

    resolved issue with 0 being output as a multiple

--- multieuler/multieuler.go
+++ multieuler/multieuler.go
@@ -3,7 +3,7 @@ package multieuler
 import "fmt"
 
 func Multiples(x int, y int, limit int) { //Multiples takes 3 int args
-	for i := 0; i <= limit; i++ { //Loops through 'limit' arg
+	for i := 1; i <= limit; i++ { //Loops through 'limit' arg
 		if i%x == 0 { //Checks if current number is a multiple of x
 			fmt.Printf("%v is a multiple of %v\n", i, x)
 		}
