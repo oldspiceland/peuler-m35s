d462821fa82b1d2c4a9a5904b1ab1db078580691
George Spiceland <georgespiceland@gmail.com>
2016-08-07 00:08:58 -0400

    fixed issues with incorrect function params declaration

--- multieuler/multieuler.go
+++ multieuler/multieuler.go
@@ -2,7 +2,7 @@ package multieuler
 
 import "fmt"
 
-func multiples(x, y, limit, int) {
+func multiples(x int, y int, limit int) {
 	for i := 0; i < limit; i++ {
 		if i%x == 0 {
 			fmt.Printf("%v is a multiple of %v\n", i, x)
