fd506cd1ed1a83930835fec41e8bb42ea300e0b1
George Spiceland <georgespiceland@gmail.com>
2016-08-06 23:59:56 -0400

    initial multieuler multiples function

--- multieuler/multieuler.go
+++ multieuler/multieuler.go
@@ -1,3 +1,12 @@
 package multieuler
 
-
+func multiples(x, y, limit, int) {
+	for i := 0; i < limit; i++ {
+		if i%x == 0 {
+			fmt.Printf("%v is a multiple of %v\n", i, x)
+		}
+		if i%y == 0 {
+			fmt.Printf("%v is a multiple of %v\n", i, y)
+		}
+	}
+}
