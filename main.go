e1c61e136cc627fca719d9dbf6827d3ce1de24a8
George Spiceland <georgespiceland@gmail.com>
2016-08-10 00:17:40 -0400

    Issue #4 Resolved, validate function added to main.go which checks for negative or 0 user input on negative or 0 user input, exits code 1

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"github.com/oldspiceland/peuler-m35s/multieuler"
 	"flag"
+	"fmt"
+	"os"
 )
 
-func main() { //TODO: Add arguments to remove hard coded limits feeding into functions
+func main() { 
 	xPtr := flag.Int("x", 3, "an int")
 	yPtr := flag.Int("y", 5, "an int")
 	limitPtr := flag.Int("limit", 1000, "an int")
 	
 	flag.Parse()
+	validate(*xPtr, *yPtr, *limitPtr)
 	multieuler.Multiples(*xPtr, *yPtr, *limitPtr)
 }
+
+func validate(x int, y int, limit int) {
+	if (x <= 0 || y <= 0 || limit <= 0) {
+		fmt.Print("Invalid Input, negative values not allowed\n")
+		os.Exit(1)
+	}
+}
+
